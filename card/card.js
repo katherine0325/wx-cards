612452ed036183ac5f041fd7d1ea44a84dcd8f05
katherine0325 <510175889@qq.com>
2019-06-29 10:59:23 +0800

    add sale-card

--- card/card.js
+++ card/card.js
@@ -10,14 +10,26 @@ Component({
     img: String,
     context: String,
     moreText: String,
+    isShowLike: Boolean,
     isLiked: {
       type: Boolean,
       observer: function () { this.setData({ isLiked: this.properties.isLiked }); }
     },
-    isShowShare: {
+    likeNumber: Number,
+    isShowUnlike: Boolean,
+    unlikeNumber: Number,
+    isUnliked: {
       type: Boolean,
-      value: true,
-    }
+      observer: function () { this.setData({ isUnliked: this.properties.isUnliked }); }
+    },
+    isShowDelete: Boolean,
+    tag: String,
+    tagColor: String,
+    // ignore the property since hide the share button
+    // isShowShare: {
+    //   type: Boolean,
+    //   value: true,
+    // }
   },
 
   /**
@@ -26,6 +38,7 @@ Component({
   data: {
     isMoreText: false,
     isLiked: false,
+    isUnlike: false,
   },
 
   /**
@@ -37,12 +50,22 @@ Component({
     },
 
     handleLike() {
-      this.setData({ isLiked: !this.data.isLiked });
-      this.triggerEvent('like');
+      this.setData({ isLiked: !this.data.isLiked, likeNumber: this.data.likeNumber++ });
+      this.triggerEvent('like', {isLiked: this.data.isLiked});
     },
 
-    handleShare() {
-      this.triggerEvent('share');
+    handleUnlike() {
+      this.setData({ isUnliked: !this.data.isUnliked });
+      this.triggerEvent('unlike', {isUnliked: this.data.isUnliked});
+    },
+
+    handleDelete() {
+      this.triggerEvent('delete');
     }
+
+    // ignore the function since the share button be hidden
+    // handleShare() {
+    //   this.triggerEvent('share');
+    // }
   }
 })
---
+++ sale-card/index.js
@@ -0,0 +1,28 @@
+// dist/cards/sale-card/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    img: String,
+    title: String,
+    price: String,
+    type: String,
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    handleDelete() {
+      this.triggerEvent('delete');
+    }
+  }
+})
